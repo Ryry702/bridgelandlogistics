c25cffbeb24f149b873bc31ea24a9ea3344c6b60
Ryan Hathaway <rryanhathaway@gmail.com>
2023-02-07 17:53:47 -0800

    contact page now has info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,17 @@
 const Contact = () => {
     return (
+<>
+        <p className="Para">Contact us</p>
 
-        <p>Contact</p>
+        <p className="Para">Email:</p>
 
+        <p className="Para">Phone: 1-250-858-2287</p>
+
+        <p className="Para">Mail: 3740 Sooke Rd, <br></br>
+        Metchosin, BC <br></br>
+        Canada
+        </p>
+</>
     );
 };
     
